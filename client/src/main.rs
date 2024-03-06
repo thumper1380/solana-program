85e73edc162fc5b75b7099ef40f30f38bc475cab
highspeed1380 <rabconnection1380@gmail.com>
2024-03-06 13:14:43 +0100

    update

--- client/src/main.rs
+++ client/src/main.rs
@@ -12,20 +12,20 @@ fn main() {
     let keypair_path = &args[1];
 
     let connection = zc::client::establish_connection().unwrap();
-    println!(
-        "Connected to remote solana node running version ({}).",
-        connection.get_version().unwrap()
-    );
+    // println!(
+    //     "Connected to remote solana node running version ({}).",
+    //     connection.get_version().unwrap()
+    // );
 
     let balance_requirement = zc::client::get_balance_requirement(&connection).unwrap();
-    println!(
-        "({}) lamports are required for this transaction.",
-        balance_requirement
-    );
+    // println!(
+    //     "({}) lamports are required for this transaction.",
+    //     balance_requirement
+    // );
 
     let player = zc::utils::get_player().unwrap();
     let player_balance = zc::client::get_player_balance(&player, &connection).unwrap();
-    println!("({}) lamports are owned by player.", player_balance);
+    // println!("({}) lamports are owned by player.", player_balance);
 
     if player_balance < balance_requirement {
         let request = balance_requirement - player_balance;
@@ -41,8 +41,8 @@ fn main() {
     zc::client::create_greeting_account(&player, &program, &connection).unwrap();
 
     zc::client::say_hello(&player, &program, &connection).unwrap();
-    println!(
-        "({}) greetings have been sent.",
-        zc::client::count_greetings(&player, &program, &connection).unwrap()
-    )
+    // println!(
+    //     "({}) greetings have been sent.",
+    //     zc::client::count_greetings(&player, &program, &connection).unwrap()
+    // )
 }
